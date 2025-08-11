1fd43eae85574b0aa5533a3ba336f128a8ceac4a
Todd Wuolle <twuolle@parcsystems.ca>
2025-08-11 10:25:35 -0700

    integrating db lambda layer

--- src/lambda/AxonCaseDetector/lambda_function.py
+++ src/lambda/AxonCaseDetector/lambda_function.py
@@ -3,6 +3,7 @@ import boto3
 import requests
 from lambda_structured_logger import LambdaStructuredLogger, LogLevel, LogStatus
 from datetime import datetime, timezone
+from bridge_tracking_db_layer import DatabaseManager, StatusCodes, get_db_manager
 
 def lambda_handler(event, context):
     # Initialize the logger
@@ -11,21 +12,32 @@ def lambda_handler(event, context):
     # Initialize AWS SSM client
     ssm_client = boto3.client('ssm')
     
+    # Initialize database manager
+    env_stage = os.environ.get('ENV_STAGE', 'dev-test')
+    db_manager = get_db_manager(env_param=env_stage)
+
     # Log the start of the function
     logger.log_start(
         event="simple_lambda_execution",
         job_id=context.aws_request_id
     )
     
+    # Get environment stage from environment variable
+    env_stage = os.environ.get('ENV_STAGE', 'dev-test')
+    logger.info(f"Loading configuration for environment: {env_stage}")
+            
+    
     # Collect SSM parameters
     try:
         # Define the parameter names you want to retrieve
+        # 'host': f'/{env_stage}/bridge/tracking-db/host',
         parameter_names = [
-            '/dev-test/axon/api/get_cases_url_filter_path',
-            '/dev-test/axon/api/authentication_url',
-            '/dev-test/axon/api/bearer',
-            '/dev-test/axon/api/client_secret',
-            '/dev-test/axon/api/client_id'
+            f'/{env_stage}/axon/api/get_cases_url_filter_path',
+            f'/{env_stage}/axon/api/authentication_url',
+            f'/{env_stage}/axon/api/bearer',
+            f'/{env_stage}/axon/api/client_secret',
+            f'/{env_stage}/axon/api/client_id',
+            f'/{env_stage}/axon/api/case_detector_interval_mins'
         ]
         
         # Retrieve multiple parameters at once
@@ -90,8 +102,11 @@ def lambda_handler(event, context):
         # Get current UTC time
         current_utc_time = datetime.now(timezone.utc)
 
+        # Get interval to use
+        case_detector_interval_mins = parameters["/dev-test/axon/api/case_detector_interval_mins"]
+
         # Substract 5 minutes to get second UTC time
-        fivemins_past = current_utc_time - timedelta(minutes=5)
+        fivemins_past = current_utc_time - timedelta(minutes=case_detector_interval_mins)
         
         # make filter string
         filter_string = f"createdOn in {fivemins_past} to {current_utc_time}"
@@ -102,20 +117,64 @@ def lambda_handler(event, context):
             }
             response = requests.get(api_url, headers=headers,params=params, timeout=10)
             response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+            
+            # Get response time
+            response_time = response.elapsed.total_seconds()
+            
+            logger.log_api_call(event="call to Axon Get Cases", method="POST", status_code= response.status_code, 
+            response_time = response_time,   job_id=context.aws_request_id)
 
             # Log success and return the response
             logger.log_success(
                 event="api_call",
                 details={"status_code": response.status_code, "url": api_url}
             )
-            return {
-                'statusCode': 200,
-                'body': json.dumps({
-                    'message': 'GET request successful',
-                    'data': response.json()  # Assuming the response is JSON
-                })
-            }
 
+            if response.status_code == 200:
+                # Parse JSON response
+                json_data = response.json()
+                 # Extract top-level meta information
+                meta = json_data.get("meta", {})
+                offset = meta.get("offset")
+                limit = meta.get("limit")
+                count = meta.get("count")
+
+                if count >= 1:
+                    data = json_data.get("data", [])
+                     # Get response time
+                    response_time = response.elapsed.total_seconds()
+            
+                    logger.log_api_call(event="call to Axon Get Cases successful. Found at least 1 case", method="POST", status_code= response.status_code, 
+                    response_time = response_time,   job_id=context.aws_request_id)
+
+                    for item in data:
+                        # Extract fields from each item
+                        item_type = item.get("type")
+                        item_id = item.get("id")
+                        attributes = item.get("attributes", {})
+
+                        # Extract attributes
+                        title = attributes.get("title")
+                        description = attributes.get("description")
+                        created_on = attributes.get("createdOn")
+                        status = attributes.get("status")
+                        owner = attributes.get("owner", {})
+                        owner_id = owner.get("id")
+                        owner_agency = owner.get("relationships", {}).get("agency", {}).get("data", {}).get("id")
+
+                        # Extract caseSharedFrom (list)
+                        case_shared_from = attributes.get("caseSharedFrom", [])
+                result = {"statusCode": 200, "body": "Success calling API, at least 1 result found."}
+                return result
+            else:
+                # Get response time
+                response_time = response.elapsed.total_seconds()
+            
+                logger.log_api_call(event="call to Axon Get Cases, no new cases found", method="POST", status_code= response.status_code, 
+                response_time = response_time,   job_id=context.aws_request_id)
+                result = {"statusCode": 200, "body": "Success calling API, no results found."}
+                return result
+            
         except requests.exceptions.RequestException as e:
             logger.log_error(
                 event="api_call_failed",
